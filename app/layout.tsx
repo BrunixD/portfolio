9b60205866b7f8d497b4b6051f68a384170637f1
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2025-10-25 20:14:54 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,12 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
